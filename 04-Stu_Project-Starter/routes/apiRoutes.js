619b3bc1424c4b4807087238e7aa0a9973e6b4d1
thnlsn <52093425+thnlsn@users.noreply.github.com>
2019-11-11 17:21:02 -0800

    finished home page api routes

--- 04-Stu_Project-Starter/routes/apiRoutes.js
+++ 04-Stu_Project-Starter/routes/apiRoutes.js
@@ -1,24 +1,40 @@
+console.log("apiRoutes.js");
+
 var db = require("../models");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  // THIS IS FOR THE HOME PAGE, ALL DATA FOR ALL SMOIS
+  app.get("/home", function(req, res) {
+    db.smoi.findAll({}).then(function(results) {
+      console.log(`Here are the results from the GET request: ${results}`);
+      res.json(results);
     });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  // THIS IS FOR WHEN CREATING A NEW SMOI, POSTING THE INPUTTED DATA TO THE DB
+  app.post("/api/create", function(req, res) {
+    db.smoi.create(req.body).then(function(results) {
+      res.json(results);
     });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  app.delete("/api/smois/:id", function(req, res) {
+    db.smoi.destroy({ where: { id: req.params.id } }).then(function(results) {
+      res.json(results);
+    });
+  });
+
+  // THIS IS FOR EACH CATEGORY, GETS ALL SMOIS FOR THE CLICKED CATEGORY
+  app.get("/api/:id", function(req, res) {
+    db.smoi.findAll({
+      where: {smoi_category: req.params.id}
+    }).then(function(results) {
+      console.log(`Here are the results from the GET request: ${results}`);
+      res.json(results);
     });
   });
 };
+
